6c48f877be2a9a63f3b06ced36ec79e1627a4af2
KEERTAN KUMAR <keertan2005@gmail.com>
2025-11-21 03:32:15 +0000

    oops_heirarchy

---
+++ oops_hierarchy.cpp
@@ -0,0 +1,35 @@
+// #include<iostream>
+// using namespace std;
+
+// class A{
+//     public:
+//    void showA() {
+//     cout<<"Class A is called"<<endl;
+//    }
+// };
+
+// class B : public A{
+//     public:
+//     void showB(){cout<<"Class B is here!"<<endl;}
+// };
+
+// class C: public A{
+//     public:
+//     void showC(){
+//         cout<<"Class C is called!"<<endl;
+//     }
+// };
+
+
+// int main(){
+//     B obj1;
+//     C obj2;
+
+//     obj1.showA();
+//     obj1.showB();
+
+//     obj2.showA();
+//     obj2.showC();
+//     return 0;
+// }
+
